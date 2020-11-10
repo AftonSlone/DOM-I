bfb29a00155b1fa5e926a00255f7f51f4eaac9e6
Afton Slone <afton.slone@gmail.com>
2020-11-09 20:46:31 -0500

    Finished Project

--- js/index.js
+++ js/index.js
@@ -1,42 +1,106 @@
 const siteContent = {
-  "nav": {
+  nav: {
     "nav-item-1": "Services",
     "nav-item-2": "Product",
     "nav-item-3": "Vision",
     "nav-item-4": "Features",
     "nav-item-5": "About",
     "nav-item-6": "Contact",
-    "img-src": "img/logo.png"
+    "img-src": "img/logo.png",
   },
-  "cta": {
-    "h1": "DOM Is Awesome",
-    "button": "Get Started",
-    "img-src": "img/header-img.png"
+  cta: {
+    h1: "DOM Is Awesome",
+    button: "Get Started",
+    "img-src": "img/header-img.png",
   },
   "main-content": {
-    "features-h4":"Features",
-    "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
-    "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "features-h4": "Features",
+    "features-content":
+      "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "about-h4": "About",
+    "about-content":
+      "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",
-    "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
-    "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
-    "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "services-h4": "Services",
+    "services-content":
+      "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "product-h4": "Product",
+    "product-content":
+      "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "vision-h4": "Vision",
+    "vision-content":
+      "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
   },
-  "contact": {
-    "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
-    "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
+  contact: {
+    "contact-h4": "Contact",
+    address: "123 Way 456 Street Somewhere, USA",
+    phone: "1 (888) 888-8888",
+    email: "sales@greatidea.io",
   },
-  "footer": {
-    "copyright" : "Copyright Great Idea! 2018"
+  footer: {
+    copyright: "Copyright Great Idea! 2018",
   },
 };
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute("src", siteContent["nav"]["img-src"]);
+
+const nav = document.querySelector("nav");
+
+const navLinks = document.querySelectorAll("nav a");
+navLinks.forEach((link, index) => {
+  link.textContent = siteContent.nav[`nav-item-${index + 1}`];
+  link.style.color = "green";
+});
+
+const logo2 = document.querySelector("#cta-img");
+logo2.setAttribute("src", siteContent.cta["img-src"]);
+
+const ctaH1 = document.querySelector(".cta h1");
+ctaH1.textContent = siteContent.cta.h1;
+
+const ctaButton = document.querySelector(".cta button");
+ctaButton.textContent = siteContent.cta.button;
+
+const textContentH4 = document.querySelectorAll(".text-content h4");
+
+const textContentP = document.querySelectorAll(".text-content p");
+
+textContentH4[0].textContent = siteContent["main-content"]["features-h4"];
+
+textContentP[0].textContent = siteContent["main-content"]["features-content"];
+
+textContentH4[1].textContent = siteContent["main-content"]["about-h4"];
+
+textContentP[1].textContent = siteContent["main-content"]["about-content"];
+
+const middleImg = document.querySelector("#middle-img");
+middleImg.src = siteContent["main-content"]["middle-img-src"];
+
+textContentH4[2].textContent = siteContent["main-content"]["services-h4"];
+
+textContentP[2].textContent = siteContent["main-content"]["services-content"];
+
+textContentH4[3].textContent = siteContent["main-content"]["product-h4"];
+
+textContentP[3].textContent = siteContent["main-content"]["product-content"];
+
+textContentH4[4].textContent = siteContent["main-content"]["vision-h4"];
+
+textContentP[4].textContent = siteContent["main-content"]["services-content"];
+
+const contactH4 = document.querySelector(".contact h4");
+
+contactH4.textContent = siteContent.contact["contact-h4"];
+
+const contactP = document.querySelectorAll(".contact p");
+
+contactP[0].textContent = siteContent.contact["address"];
+
+contactP[1].textContent = siteContent.contact["phone"];
+
+contactP[2].textContent = siteContent.contact["email"];
+
+const footerP = document.querySelector("footer p");
+footerP.textContent = siteContent.footer.copyright;
